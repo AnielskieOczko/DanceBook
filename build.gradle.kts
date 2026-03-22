37b939e95d1331e618a245e32af31efb2d089cdb
Rafal Jankowski <rafaljankowski7@gmail.com>
2026-03-22 14:36:47 +0100

    Added: 1) .dockerignore 2) Removed unused imports 3) Added new dependency springboot validation

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
     implementation("org.flywaydb:flyway-core")
     implementation("org.flywaydb:flyway-database-postgresql")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation(".org.springframework.boot:spring-boot-starter-validation")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     developmentOnly("org.springframework.boot:spring-boot-docker-compose")
     runtimeOnly("org.postgresql:postgresql")
